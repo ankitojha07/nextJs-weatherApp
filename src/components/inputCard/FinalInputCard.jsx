2525706e3d6a114a9f3882470ff27267d529b89c
Ankit <ankitojha1409@gmail.com>
2024-07-16 23:43:54 +0530

    Added API calls and State management in react sj useState

--- src/components/inputCard/FinalInputCard.jsx
+++ src/components/inputCard/FinalInputCard.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FinalInputCard = () => {
+const FinalInputCard = ({ onSearch }) => {
+  const [city, setCity] = useState("");
+
+  const handleSearch = () => {
+    onSearch(city);
+  };
+
   return (
     <div className="md:h-[300px] md:w-[500px] w-full bg-white p-8 flex flex-col justify-center items-center rounded-3xl gap-3">
       <p className="text-white bg-orange-600 px-4 py-1 rounded-full">
@@ -9,7 +15,6 @@ const FinalInputCard = () => {
       <p className="md:text-4xl text-3xl">
         <span className="font-bold ">Weather</span> APP
       </p>
-
       <div className="flex flex-col justify-center items-center gap-4 text-center">
         <div className="flex flex-col justify-center items-center">
           <p className="font-extralight">
@@ -22,10 +27,13 @@ const FinalInputCard = () => {
             className="px-4 py-2 border rounded-xl md:rounded-s-2xl"
             type="text"
             placeholder="Enter your city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
           />
           <button
             id="searchBtn"
             className="bg-blue-600 text-white px-4 py-2 md:rounded-e-2xl rounded-xl w-full"
+            onClick={handleSearch}
           >
             Search
           </button>
--- src/components/outputCard/FinalOutput.jsx
+++ src/components/outputCard/FinalOutput.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 
-const FinalOutput = () => {
+const FinalOutput = ({ weatherData }) => {
+  if (!weatherData) {
+    return null;
+  }
+
   return (
-    <div className="md:h-[300px] md:w-[500px] w-full bg-white p-8 flex-col justify-center items-center rounded-3xl md:gap-8 gap-4 hidden">
-      <div id="outputCard" className="flex flex-col gap-4">
+    <div className="md:h-[200px] md:w-[500px] w-full bg-white p-8 flex-col justify-center items-center rounded-3xl md:gap-8 gap-4">
+      <div
+        id="outputCard"
+        className="flex flex-col gap-4 justify-center items-center"
+      >
         <div className="flex flex-row gap-4 items-center">
           <p id="city-name" className="font-bold">
-            New Delhi
+            {weatherData.location.name}
           </p>
           <p id="countryName" className="bg-orange-400 px-4 py-1 rounded-3xl">
-            India
+            {weatherData.location.country}
           </p>
         </div>
-        <div className="flex flex-row text-5xl font-bold">23 • C</div>
+        <div className="flex flex-row text-5xl font-bold">
+          {weatherData.current.temp_c} °C
+        </div>
         <div id="loc-time" className="italic">
-          22-08-2024 23:00
+          {weatherData.location.localtime}
         </div>
       </div>
     </div>
